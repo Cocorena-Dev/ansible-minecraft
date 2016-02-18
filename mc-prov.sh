258a2c13dfd9f789b0e02faa8c160f7f60055d39
dev@cocorena.com <dev@cocorena.com>
2016-02-18 03:35:36 -0600

    added map server

---
+++ mc-prov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This is the master provisioning script
+
+#provision nodes
+ansible-playbook config/mc-prov.yml -f 20
+
+#configure nodes
+ansible-playbook -i scripts/rax.py config/mc-config.yml -f 20
+
+#ping them
+ansible -vvvv -u root -i scripts/rax.py mc -m ping -f 20
