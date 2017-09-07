213ec004f3fc7c6c76da6eb238c6a321e30c702a
cocorenadev <dev@cocorena.net>
2017-09-07 09:43:02 +0000

    updating playbooks 1.12.1

--- mc-prov.sh
+++ mc-prov.sh
@@ -5,7 +5,7 @@
 ansible-playbook -i scripts/rax.py config/mc-prov.yml -f 20
 
 #configure nodes
-ansible-playbook -i scripts/rax.py config/mc-config.yml -f 20
+ansible-playbook -vvvv -i scripts/rax.py config/mc-config.yml -f 20
 
 #ping them
 ansible -vvvv -u root -i scripts/rax.py mc -m ping -f 20
--- web-quickrender.sh
+++ web-quickrender.sh
@@ -3,13 +3,13 @@
 rm -rf ~/.ansible/
 
 #provision nodes
-ansible-playbook config/web-prov.yml -f 20
+/usr/local/bin/ansible-playbook /home/dev/ansible-minecraft/config/web-prov.yml -f 20
 
 #Ansible Ping entire environment
-ansible -u root -i scripts/rax.py web -m ping -f 20
+/usr/local/bin/ansible -u root -i /home/dev/ansible-minecraft/scripts/rax.py web -m ping -f 20
 
 #configure nodes
-ansible-playbook -i scripts/rax.py config/web-config.yml -f 20 --skip-tags "fullrender"
+/usr/local/bin/ansible-playbook -i /home/dev/ansible-minecraft/scripts/rax.py /home/dev/ansible-minecraft/config/web-config.yml -f 20 --skip-tags "fullrender"
 
 #Delete node after job runs.
-ansible-playbook -i scripts/rax.py config/web-delete.yml -f 20
+/usr/local/bin/ansible-playbook -i /home/dev/ansible-minecraft/scripts/rax.py /home/dev/ansible-minecraft/config/web-delete.yml -f 20
